05cdcc4119b6af933cdeb51657afe393ce969961
Delbel <rafaeldelbel@live.com>
2014-06-04 00:52:12 -0300

    implementacao hibernate

--- src/main/java/br/com/rango/controller/InicialController.java
+++ src/main/java/br/com/rango/controller/InicialController.java
@@ -1,15 +1,37 @@
 package br.com.rango.controller;
 
+import java.math.BigDecimal;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.rango.dao.RefeicaoDAO;
+import br.com.rango.model.Refeicao;
+
 @Controller
-@RequestMapping(value = "/inicial")
+@RequestMapping
 public class InicialController {
+	@Autowired
+	private RefeicaoDAO refeicaoDAO;
 	
-	@RequestMapping
+	@RequestMapping(value = "/inicial")
+	@Transactional
 	public String paginaInicial() {
-		return "principal/inicial";
+		Refeicao ref = new Refeicao();
+		ref.setMetodoPagamento("cartao");
+		ref.setRefeicaoId(1L);
+		ref.setEntidade("andromeda");
+		ref.setValor(BigDecimal.valueOf(7.50));
+		refeicaoDAO.save(ref);
+		return "/inicial";
+	}
+	
+	@RequestMapping(value = "/buscar")
+	public String buscar() {
+		return "/buscar";
 	}
 	
 }
